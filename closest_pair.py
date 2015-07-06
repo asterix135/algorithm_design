3180514d76bad1689aef710bf95ca819be9f31b8
asterix135 <chgraham@gmail.com>
2015-07-06 08:17:48 -0400

    minor additions

---
+++ closest_pair.py
@@ -0,0 +1,17 @@
+'''routine to calculate closest set of pairs in a 2-d plane'''
+
+## setup stuff
+import time
+import math
+
+def euc_distance(point1, point2):
+    return math.sqrt((point1[0] - point2[0])**2 + (point1[1] - point2[1])**2)
+
+def brute_force(lst):
+    pass
+
+def closest_pair(lst):
+    pass
+
+def test():
+    pass
--- matrix_multiplication.py
+++ matrix_multiplication.py
@@ -5,12 +5,28 @@ Will throw an error if these conditions are not met'''
 import time
 
 def brute_force(x_matrix, y_matrix):
+    '''brute force calculation of matrix multiplication
+    assumes 2 square matrices and does not check for this'''
     start = time.time()
-    z_matrix = []
-    for row in len(x_matrix):
-        for element in row:
+
+    ## create empty output matrix - works for different sizes
+    z_matrix = [[0 for dummycol in range(len(x_matrix))]
+                           for dummyrow in range(len(y_matrix[0]))]
+
+    for xrow in len(x_matrix):
+        for ycol in len(y_matrix[0]):
+            for yrow in len(y_matrix):
+                z_matrix[x_row, yxol] = 0 ### work this out
 
 
     print (time.time() - start)
     return z_matrix
 
+def matrix_multiplication(x_matrix, y_matrix):
+    pass
+
+def gen_random_square_matrix(size):
+    pass
+
+def test():
+    pass
