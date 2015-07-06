f334fbb541c543ebe31e984860b332be45491876
asterix135 <chgraham@gmail.com>
2015-07-05 22:30:04 -0400

    started matrix_multiplication algorithm

---
+++ matrix_multiplication.py
@@ -0,0 +1,16 @@
+'''approaches to multiplying two SQUARE matrices with identical dimensions
+Will throw an error if these conditions are not met'''
+
+## setup stuff
+import time
+
+def brute_force(x_matrix, y_matrix):
+    start = time.time()
+    z_matrix = []
+    for row in len(x_matrix):
+        for element in row:
+
+
+    print (time.time() - start)
+    return z_matrix
+
