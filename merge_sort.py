a0350523c9248ed1097f7b1926d451b6802dee9b
asterix135 <chgraham@gmail.com>
2015-07-07 21:05:56 -0400

    updated m/s to deal with lists of tuples or lists

--- merge_sort.py
+++ merge_sort.py
@@ -1,42 +1,42 @@
 '''Functions to implement Merge/sort algorithm'''
 
-import random
-LIST_LENGTH = 10000
-RANDOM_LIST = random.sample(range(LIST_LENGTH), LIST_LENGTH)
-# RANDOM_LIST = [4,3,2,1]
 
-def split_sort(number_list):
-    '''Takes a list and sorts it in ascending order'''
-    if len(number_list) == 2:
-        if number_list[0] > number_list[1]:
-            return [number_list[1], number_list[0]]
+def merge_sort(lst, idx = 0):
+    '''Main merge/sort function to call
+    takes a list as input and sorts in numerical order
+    can take an optional idx parameter, if the elements of the list are iterable - will sort on index position'''
+    if len(lst) == 2:
+        if lst[0][idx] > lst[1][idx]:
+            return [lst[1], lst[0]]
         else:
-            return [number_list[0], number_list[1]]
-    elif len(number_list) == 1:
-        return number_list
+            return lst
+    elif len(lst) <= 1:
+        return lst
     else:
-        output_list = []
-        list_a = split_sort(number_list[0:int(len(number_list)/2)])
-        list_b = split_sort(number_list[int(len(number_list)/2):])
-        list_a_position = 0
-        list_b_position = 0
-        while list_a_position + list_b_position <len(number_list):
-            if list_a[list_a_position] < list_b[list_b_position]:
-                output_list.append(list_a[list_a_position])
-                list_a_position += 1
-                if list_a_position == len(list_a):
-                    output_list.extend(list_b[list_b_position:])
-                    list_a_position = len(number_list)
-            else:
-                output_list.append(list_b[list_b_position])
-                list_b_position += 1
-                if list_b_position == len(list_b):
-                    output_list.extend(list_a[list_a_position:])
-                    list_b_position = len(number_list)
-
+        list_a = merge_sort(lst[:len(lst)//2], idx)
+        list_b = merge_sort(lst[len(lst)//2:], idx)
+        output_list = merge(list_a, list_b, idx)
         return output_list
 
+def merge(list_a, list_b, idx):
+    '''merge subroutine called from merge_sort
+    takes two sorted sub-lists and merges them into one'''
+    output_list = []
+    list_a_position = 0
+    list_b_position = 0
+    while list_a_position + list_b_position < len(list_a) + len(list_b):
+        if list_a[list_a_position][idx] < list_b[list_b_position][idx]:
+            output_list.append(list_a[list_a_position])
+            list_a_position += 1
+            if list_a_position == len(list_a):
+                output_list.extend(list_b[list_b_position:])
+                list_b_position = len(list_b)
+        else:
+            output_list.append(list_b[list_b_position])
+            list_b_position += 1
+            if list_b_position == len(list_b):
+                output_list.extend(list_a[list_a_position:])
+                list_a_position = len(list_a)
+    return output_list
 
 
-test_result = split_sort(RANDOM_LIST)
-print (test_result[0], test_result[len(test_result) -1], test_result[int(len(test_result)/2)])
