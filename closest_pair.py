81aff188b3286efe20719e9d0ba66fbe3691ab95
asterix135 <asterix135@users.noreply.github.com>
2015-07-09 17:49:53 -0400

    started rework; cp_split good

--- closest_pair.py
+++ closest_pair.py
@@ -19,52 +19,50 @@ def brute_force(lst):
                 closest_pair[0] = lst[point1]
                 closest_pair[1] = lst[point2]
     print ('brute force elapsed time: ' + str(time.time() - start))
-    return [closest_pair, closest_distance]
+    return [closest_distance, closest_pair]
 
 
 def closest_pair(lst):
     start = time.time()
-    list_x_sort = ms.merge_sort(lst, 0)
-    list_y_sort = ms.merge_sort(lst, 1)
-    best = float('inf')
+    x_sort = ms.merge_sort(lst, 0)
+    y_sort = ms.merge_sort(lst, 1)
+    solution = cp_split(x_sort, y_sort)
+    print ("closest pair run time: " + str(time.time() - start))
+    return solution
+
+def cp_split(x_list, y_list):
+    if len(x_list) <= 3:
+        return brute_force(x_list)
+    else:
+        x_left = x_list[:len(x_list)//2]
+        x_right = x_list[len(x_list)//2:]
+        x_bar = x_left[-1]
+        y_left = [val for val in y_list if val[0] <= x_bar[0]]
+        y_right = [val for val in y_list if val[0] > x_bar[0]]
+        left_best = cp_split(x_left, y_left)
+        right_best = cp_split(x_right, y_right)
+        if left_best[0] < right_best[0]:
+            delta = left_best[0]
+            best_pair = left_best[1]
+        else:
+            delta = right_best[0]
+            best_pair = right_best[1]
+        split_best = closest_split_pair(x_list, y_list, delta, best_pair)
+        if delta > split_best[0]:
+            delta = split_best[0]
+            best_pair = split_best[1]
+        return [delta, best_pair]
 
 
-    ## Approach
-    ## merge-sort list by x-coordinate (Px)
-    ## merge-sort list by y-coordinate (Py)
+def closest_split_pair(x_list, y_list, delta, best_pair):
 
-    ## form Qx Qy, Rx Ry (somehow use Px & Py - linear time)
-    ## get closest pair in each half (p1, q1) = closest in Q; (p2, q2) = closest in R
-    ## calculate delta - min of dist(p1, q1) and dist(p2, q2)
-    ## calculate closest split pair (p3, q3) & return closest of (p1, q1), (p2, q2) and (p3, q3)
     ## closest split pair takes 3 parameters - delta, Px & Py
     ##
-    final_pair = cp_split(lst)
-    print ("closest pair run time: " + str(time.time() - start))
-    return final_pair
 
-def cp_split(lst):
-    if len(lst) == 2:
-        return (lst[0], lst[1])
-    elif len(lst) == 3:
-        dist1 = (euc_distance(lst[0], lst[1]), (lst[0], lst[1]))
-        dist2 = (euc_distance(lst[0], lst[2]), (lst[0], lst[2]))
-        dist3 = (euc_distance(lst[1], lst[2]), (lst[1], lst[2]))
-        return min(dist1, dist2, dist3)[1]
-    else:
-        left_closest = cp_split(lst[:len(lst)//2])
-        right_closest = cp_split(lst[len(lst)//2:])
-        print ('left closest ' + str(left_closest))
-        delta = min(euc_distance(left_closest[0], left_closest[1]),
-                    euc_distance(right_closest[0], right_closest[1]))
-        split_closest = closest_split_pair(lst[:len(lst)//2], lst[len(lst)//2 :], delta)
-        return min(euc_distance(left_closest[0], left_closest[1]),
-                   euc_distance(right_closest[0], right_closest[1]),
-                   euc_distance(split_closest[0], split_closest[1]))
+    ## cf: http://stackoverflow.com/questions/28237581/closest-pair-implemetation-python
+    ## http://rosettacode.org/wiki/Closest-pair_problem
 
-def closest_split_pair(lst1, lst2, delta):
-    best_pair = ((float('inf'), float('inf')), (float('-inf'), float ('-inf')))
-    best = float('inf')
+
     ## xbar = biggest x-value  in left half of P
     ## Sy = points with x-coordinate in range (x-bar - delta, x-bar + delta), sorted by y-coordinate
     ## initialize variables best = delta, best_pair = null
@@ -74,17 +72,17 @@ def closest_split_pair(lst1, lst2, delta):
     ##      if dist(p,q) < best
     ##          best = dist(p,q); best_pair = (p,q)
     ## return best_pair or null
-    print (best_pair)
-    return best_pair
+    print ('best pair ' + str(best_pair))
+    return [delta, best_pair]
 
 def test():
     test_list = [(0, 10), (20, 11), (10, 0), (11, 20), (9,9), (11,11)]
     result = brute_force(test_list)
-    print ('closest pair is: ' + str(result[0]) + '.  Distance is: ' + str(result[1]))
+    print ('brute force result: ' + str(result[1]) + '.  Distance is: ' + str(result[0]))
     result2 = closest_pair(test_list)
-    print (result2)
+    print ('closest pair test 1 ' + str(result2))
     test_list2 = [(0, 10), (20, 11), (10, 0), (11, 20), (9,9), (11,11), (44,44), (99,99)]
-    print (closest_pair(test_list2))
+    print ('closest pair test 2 ' + str(closest_pair(test_list2)))
 
 
 
