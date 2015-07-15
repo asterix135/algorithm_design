1de6ea140d9f46473eef373c522469af036db045
asterix135 <chgraham@gmail.com>
2015-07-15 07:54:41 -0400

    partition not working

--- quicksort.py
+++ quicksort.py
@@ -2,47 +2,46 @@
 
 import random
 
-def quick_sort(array):
-    ## This may need to go into function parameters
-    length = len(array)
-    if length == 1:
-        return array
-    pivot = random.randrange(length)
-    semi_sort = partition(array, left, right)
+def sort_array(array):
+    quick_sort(array, 0, len(array))
+
+def quick_sort(array, left, right):
+    if right - left <= 0:
+        return
+    pivot = partition(array, left, right)
+    quick_sort(array, left, pivot-1)
+    quick_sort(array, pivot + 1, right)
+    return array
 
 
 def partition(array, left, right):
-    pivot = array[left]
-    sort_split = left + 1
+    pivot_idx = random.randrange(left, right)
+    pivot = array[pivot_idx]
+    if pivot_idx != left:
+        array[left], array[pivot_idx] = array[pivot_idx], array[left]
+    # split_idx refers to the break between higher than pivot & less than pivot
+    split_idx = left + 1
+    # sort_idx refers to the break between sorted & unsorted elements
+    sort_idx = left + 1
+    # while sort_idx < right:
 
-    ## not sure if range ends at right or right + 1
-    for unsorted in range(length + 1, right + 1):
-        if array[break_point] < pivot:
-            array[sort_split], array[unsorted] = array[unsorted], \
-                                                 array[sort_split]
-            sort_split += 1
-    array[pivot], array[sort_split -1] = array[sort_split - 1], array[pivot]
-
-
-# QuickSort(array, length n)
-#      if n = 1, return
-#      p = choose pivot (A, n) - running time depends on this choice
-#      partition A around p
-#      recursively sort on both sides of p
-# no combine or merge step
-#
-# Partition (A, l, r)
-#      l & r indicate indices of the left-most index (where you're supposed to work) & right-most (boundary between sorted & unsorted??) = l & r bound the sub-array you're working on
-#      pi = A[l]
-#      i = l + 1
-#      for j = l+1 to r:
-#           if A[j] > p: do nothing
-#           if A[j} < p:
-#                Swap A[j] and A[i]
-#                     (this will introduce redundant swaps until you find
-#                      a result > p)
-#                i += 1
-#      Swap A[l] and A[i-1]
+    for sort_idx in range(left + 1, right):
+        if array[sort_idx] < pivot:
+            array[sort_idx], array[split_idx] = \
+                array[split_idx], array[sort_idx]
+            split_idx += 1
+        sort_idx += 1
+    array[left], array[split_idx - 1] = array[split_idx - 1], array[left]
+    return split_idx -1
 
 
-quick_sort([4,2,3,1])
+
+def test_routine(array_len):
+    input_array = random.sample(range(array_len), array_len)
+    sort_array(input_array)
+    return input_array
+
+
+print (test_routine(10))
+
+# print (test_routine(3))
