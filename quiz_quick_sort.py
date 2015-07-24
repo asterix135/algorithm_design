c8464b0420f1d212daabed0134f0105dead077b2
asterix135 <chgraham@gmail.com>
2015-07-25 08:40:46 +0900

    homework question working

--- quiz_quick_sort.py
+++ quiz_quick_sort.py
@@ -1,40 +1,40 @@
 '''implemetation of quick sort algorithm for programming questions'''
 
-# import random
-import urllib.request
-
 m_count = 0
 
-def sort_array(array, sub_option):
-    ## note - you have to make the starting right len(array) - 1 for the
-    ## for loop to work correctly in partition
+def sort_array(array, sub_option=3):
+    """
+    Main call - sub_option refers to problem number in homeowork
+    array is actually a list
+    """
     quick_sort(array, 0, len(array)-1, sub_option)
 
 def quick_sort(array, left, right, sub_option):
+    """
+    primary recursive algorithm
+    """
     global m_count
-    m_count +=  abs(right - left)
+    if right >= left:
+        m_count +=  right - left
     if right - left <= 0:
         return
     pivot = partition(array, left, right, sub_option)
-    print ('left: ' + str(left) + ', pivot-1: ' + str(pivot-1) + ' m: ' + str(m_count))
     quick_sort(array, left, pivot - 1, sub_option)
     quick_sort(array, pivot + 1, right, sub_option)
 
 
-    # if m_count == 9:
-    #     quick_sort(array,left, pivot-1, sub_option)
-    #     quick_sort(array, pivot+1, right, sub_option)
-
-
-
 def partition(array, left, right, sub_option):
+    """
+    Sort elements before and after pivot.  Pivot point is determined by
+    sub-option: 1 = fist element in list,  2 = last item in list
+    3 = median of first, last & median element in list
+    """
     if sub_option == 2:
-        print (array[right])
         array[left], array[right] = array[right], array[left]
     elif sub_option == 3:
         left_val = array[left]
         right_val = array[right]
-        mid_idx = (right - left) // 2
+        mid_idx = ((right - left) // 2) + left
         mid_val = array[mid_idx]
         if left_val < mid_val < right_val or right_val < mid_val < left_val:
             array[left], array[mid_idx] = array[mid_idx], array[left]
@@ -51,65 +51,77 @@ def partition(array, left, right, sub_option):
             split_idx += 1
         sort_idx += 1
     array[left], array[split_idx - 1] = array[split_idx - 1], array[left]
-    print (str(m_count) + ' ' + str(array))
     return split_idx -1
 
 
 
 def test_routine():
+    """Verification tests"""
     global m_count
     data1 = [int(line.strip()) for line in open('test_10.txt', 'r')]
     m_count = 0
-    print (data1)
     sort_array(data1, 1)
-    print (data1)
     print (str(m_count) + ' should be 25')
-    # data1 = [int(line.strip()) for line in open('test_10.txt', 'r')]
-    # print (data1)
-    # m_count = 0
-    # sort_array(data1, 2)
-    # print (data1)
-    # print (str(m_count) + ' should be 29')
-    # data1 = [int(line.strip()) for line in open('test_10.txt', 'r')]
-    # print (data1)
-    # m_count = 0
-    # sort_array(data1, 3)
-    # print (data1)
-    # print (str(m_count) + ' should be 21')
+    data1 = [int(line.strip()) for line in open('test_10.txt', 'r')]
+    m_count = 0
+    sort_array(data1, 2)
+    print (str(m_count) + ' should be 29')
+    data1 = [int(line.strip()) for line in open('test_10.txt', 'r')]
+    m_count = 0
+    sort_array(data1, 3)
+    print (str(m_count) + ' should be 21')
+    print()
 
 
-    # data2 = [int(line.strip()) for line in open('test_100.txt', 'r')]
-    # m_count = 0
-    # sort_array(data2, 1)
-    # print (str(m_count) + ' should be 615')
-    # data2 = [int(line.strip()) for line in open('test_100.txt', 'r')]
-    # m_count = 0
-    # sort_array(data2, 2)
-    # print (str(m_count) + ' should be 587')
-    # data2 = [int(line.strip()) for line in open('test_100.txt', 'r')]
-    # m_count = 0
-    # sort_array(data2, 3)
-    # print (str(m_count) + ' should be 518')
-    #
-    #
-    # data3 = [int(line.strip()) for line in open('test_1000.txt', 'r')]
-    # m_count = 0
-    # sort_array(data3, 1)
-    # print (str(m_count) + ' should be 10297')
-    # data3 = [int(line.strip()) for line in open('test_1000.txt', 'r')]
-    # m_count = 0
-    # sort_array(data3, 2)
-    # print (str(m_count) + ' should be 10184')
-    # data3 = [int(line.strip()) for line in open('test_1000.txt', 'r')]
-    # m_count = 0
-    # sort_array(data3, 3)
-    # print (str(m_count) + ' should be 8921')
+    data2 = [int(line.strip()) for line in open('test_100.txt', 'r')]
+    m_count = 0
+    sort_array(data2, 1)
+    print (str(m_count) + ' should be 615')
+    data2 = [int(line.strip()) for line in open('test_100.txt', 'r')]
+    m_count = 0
+    sort_array(data2, 2)
+    print (str(m_count) + ' should be 587')
+    data2 = [int(line.strip()) for line in open('test_100.txt', 'r')]
+    m_count = 0
+    sort_array(data2, 3)
+    print (str(m_count) + ' should be 518')
+    print()
 
 
+    data3 = [int(line.strip()) for line in open('test_1000.txt', 'r')]
+    m_count = 0
+    sort_array(data3, 1)
+    print (str(m_count) + ' should be 10297')
+    data3 = [int(line.strip()) for line in open('test_1000.txt', 'r')]
+    m_count = 0
+    sort_array(data3, 2)
+    print (str(m_count) + ' should be 10184')
+    data3 = [int(line.strip()) for line in open('test_1000.txt', 'r')]
+    m_count = 0
+    sort_array(data3, 3)
+    print (str(m_count) + ' should be 8921')
 
 
+def homework_problem():
+    """
+    Runs quicksort & returns number of comparisons on homework list
+    """
+    global m_count
+    quiz_file = [int(line.strip()) for line in open("QuickSort.txt", 'r')]
+    m_count = 0
+    sort_array(quiz_file, 1)
+    print('Question One Answer: ' + str(m_count) + '\n')
 
-print (test_routine())
+    quiz_file = [int(line.strip()) for line in open("QuickSort.txt", 'r')]
+    m_count = 0
+    sort_array(quiz_file, 2)
+    print('Question Two Answer: ' + str(m_count) + '\n')
 
-# quiz_file = [int(line.strip()) for line in open("QuickSort.txt", 'r')]
+    quiz_file = [int(line.strip()) for line in open("QuickSort.txt", 'r')]
+    m_count = 0
+    sort_array(quiz_file, 3)
+    print('Question Three Answer: ' + str(m_count) + '\n')
 
+# test_routine()
+
+homework_problem()
