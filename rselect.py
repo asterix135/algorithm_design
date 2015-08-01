f22634f0ebe96414c27aedd3e56b2950952a6ce9
asterix135 <chgraham@gmail.com>
2015-08-01 17:18:41 +0900

    rselect working

--- rselect.py
+++ rselect.py
@@ -24,19 +24,21 @@ def rselect(array, istat):
     if len(array) == 1:
         return array[0]
     else:
-        pivot = partition(array)
+        array_copy = list(array)
+        pivot = partition(array_copy)
         if pivot == istat:
-            return array[pivot]
+            return array_copy[pivot]
         elif pivot > istat:
-            return rselect(array[:pivot-1], istat)
+            return rselect(array_copy[:pivot], istat)
         else:
-            return rselect(array[pivot+1:], istat-pivot)
+            return rselect(array_copy[pivot+1:], istat-pivot-1)
 
 
 def partition(array):
     """
     partition array to find ith statistic
     """
+    # random.seed(0)
     pivot_idx = random.randrange(len(array))
     pivot = array[pivot_idx]
     if pivot_idx != 0:
@@ -54,68 +56,31 @@ def partition(array):
     return split_idx -1
 
 
-
-
-
-# import quicksort #maybe to re-use partition
-#
-#
-# def select_ith_order(array, istat):
-#     """
-#     main routine to find ith order statistic of an array
-#     takes array (list) as input
-#     returns ith order statistic
-#     ith order counted in normal, not pythonic way
-#     Will not modify original array
-#     """
-#     array_copy = list(array)
-#     return rselect(array_copy, 0, len(array_copy) -1, istat -1)
-#
-#
-#
-# def rselect(array, left, right, istat):
-#     """
-#     Algorithm to return ith order element of an array
-#
-#     Pseudocode:
-#     RSelect(array A, length n, order statistic i)
-#      if n=1, return A[1]
-#      else
-#           choose pivot p uniformly at random
-#           partition A around p
-#           let j = position of pivot in array (pivot is jth order statistic)
-#           if i == j
-#                return p
-#           if j > i
-#                return RSelect (1st part of A, j-1, i)
-#           if j < i
-#                return RSelect (2nd part of A, n-j, i-j)
-#     """
-#     if right-left <= 0:
-#         return array[left:right+1][0]
-#     else:
-#         pivot = quicksort.partition(array, left, right)
-#         if pivot == istat:
-#             return pivot
-#         elif pivot > istat:
-#             a = rselect(array, left, pivot-1, istat)
-#             print(a)
-#             return a
-#             # return rselect(array, left, pivot-1, istat)
-#         else:
-#             a = rselect(array, pivot + 1, right, istat - pivot)
-#             print(a)
-#             return a
-#             # return rselect(array, pivot + 1, right, istat - pivot)
-#
-#
-#
-#
 def test():
     """Testing routine"""
-    array = [1,5,3,2,4,6,0,7]
-    # array = [2,4,0,3,1]
-    print(rselect(array, 5))
-    print(array)
+    array = [1,5,3,2,4,6,0,7,8]
+    i_element = 4
+    # array = [2,0,1]
+    print(rselect(array, i_element))
+    import quicksort
+    quicksort.sort_array(array)
+    print(array[i_element])
 
-test()
+def random_test(array_len):
+    """Test on randomized array"""
+    input_array = random.sample(range(array_len), array_len)
+    i_element = array_len // 2
+    print("algorithm result")
+    print(rselect(input_array, i_element))
+    import quicksort
+    quicksort.sort_array(input_array)
+    print('quicksort results')
+    print(input_array[i_element])
+
+def random_test_battery():
+    for i in range(5,5006,1000):
+        print("array length: " + str(i))
+        random_test(i)
+        print()
+
+# random_test_battery()
