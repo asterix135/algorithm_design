8a34f172f4a81b646f349030204ddf22b82e2ace
asterix135 <chgraham@gmail.com>
2015-09-09 08:33:36 -0400

    needs work

--- assignment4.py
+++ assignment4.py
@@ -1,20 +1,41 @@
 import kosaraju
-# import sys
-# import resource
-
-
-# sys.setrecursionlimit(10**6)
-# resource.setrlimit(resource.RLIMIT_STACK, (2 ** 29, 2 ** 30))
+import collections
 
 
 def read_in_graph():
+    """
+    reads in a text respresentation of a graph where each line consists
+    of tail and head of an edge
+    """
     problem_graph = kosaraju.gc.KjGraph('d')
-    with open("SCC.txt") as file_in:
+    # filename = "SCC.txt"
+    filename = "test_scc.txt"
+    with open(filename) as file_in:
         for line in file_in:
             val = line.strip().split()
             tail, head = int(val[0]), int(val[1])
             problem_graph.add_edge(tail, head)
     return problem_graph
+
+
+def top5_sccs(graph):
+    """
+    Finds top 5 SCCs by size in a graph that has been submitted to
+    kosaraju's algorithm
+    """
+    scc_sizes = {}
+    for node in graph:
+        scc_id = node.get_leader()
+        if scc_id in scc_sizes:
+            scc_sizes[scc_id] += 1
+        else:
+            scc_sizes[scc_id] = 1
+    sorted_scc_sizes = collections.OrderedDict(sorted(scc_sizes.items(),
+                                                      key=lambda t: -t[1]))
+    top5 = []
+    for item in range(min(5, len(scc_sizes))):
+        top5.append(sorted_scc_sizes.popitem(0))
+    return top5
 
 
 if __name__ == "__main__":
@@ -23,3 +44,4 @@ if __name__ == "__main__":
     print("finished read in")
     print()
     kosaraju.kosaraju(problem_graph)
+    print(top5_sccs(problem_graph))
